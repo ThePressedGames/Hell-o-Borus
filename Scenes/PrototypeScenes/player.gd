617351d15135332f3f06205390c30443706ecba2
Marco Dugatto <dughi.boogie@gmail.com>
2024-01-19 20:42:30 +0100

    Added player automovement and new animations

--- Scenes/PrototypeScenes/player.gd
+++ Scenes/PrototypeScenes/player.gd
@@ -9,13 +9,32 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	$AnimatedSprite2D.play()
-	
-	# Set initial camera left boundary
-	var current_camera_left_boundary = position.x - (get_viewport_rect().size.x/2)
-	$Camera2D.set_limit(SIDE_LEFT, current_camera_left_boundary)
+
+
+func _process(delta):
+	position.x += delta * SPEED
 
 
 func _physics_process(delta):
+	if is_on_floor():
+		$AnimatedSprite2D.animation = "run"
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("move-left"):
+		direction.x -= .5
+		$Camera2D.position.x += 5
+	if Input.is_action_pressed("move-right"):
+		direction.x += .7
+		$Camera2D.position.x -= 7
+		
+	if direction != Vector2.ZERO:
+		velocity.x = direction.x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -23,30 +42,11 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Vector2.ZERO
-	
-	if Input.is_action_pressed("move-left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move-right"):
-		direction.x += 1
-		
-		var camera_left_limit = $Camera2D.get_limit(SIDE_LEFT)
-		var current_camera_left_boundary = position.x - (get_viewport_rect().size.x/2)
-		
-		# Set new camera limit_left
-		if camera_left_limit < current_camera_left_boundary:
-			$Camera2D.set_limit(SIDE_LEFT, current_camera_left_boundary)
-	
-	if direction != Vector2.ZERO:
-		velocity.x = direction.x * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.animation = "jump"
 	
 	move_and_slide()
 
 
 func _on_obstacle_body_entered(body):
 	print("Obstacle hit!")
+	$AnimatedSprite2D.animation = "death"
