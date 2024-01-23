f7d8da0ee8e885e8c8bf5ae5b78aca1f7619e512
Marco Dugatto <dughi.boogie@gmail.com>
2024-01-23 17:15:33 +0100

    Added spring animation

--- Scenes/Prototype/spreading_spring.gd
+++ Scenes/Prototype/spreading_spring.gd
@@ -1,8 +1,15 @@
 extends LethalObstacle
 
 
-@export var speed:int = 400
+@export var speed:int = 800
 
+
+func _ready():
+	$AnimatedSprite2D.play()
 
 func _process(delta):
 	position.x = position.x + (delta * speed)
+
+
+func _on_player_hit():
+	speed = 0
