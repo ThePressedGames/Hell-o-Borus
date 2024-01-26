d5c57356d50ccb32538a6de5c0021ec5a9874ef3
Marco Dugatto <dughi.boogie@gmail.com>
2024-01-26 19:15:41 +0100

    Added Credits panel to main menu

--- Scenes/Prototype/main_menu.gd
+++ Scenes/Prototype/main_menu.gd
@@ -1,6 +1,11 @@
 extends Control
 
 
+@export var borus_click_sfx: Array[AudioStreamMP3]
+var borus_normal_sprite = preload("res://Art/PC/Borus_normal.png")
+var borus_sweat_sprite = preload("res://Art/PC/Borus_sweat.png")
+
+
 func _ready():
 	SilentWolf.configure({
 		"api_key": "lb6MlBbxsM5CkXwPi710p7pCzWeDVExd3TIjymaa",
@@ -10,6 +15,7 @@ func _ready():
 	})
 	
 	$LeaderboardPanel.hide()
+	$CreditsPanel.hide()
 
 
 func _on_play_button_pressed():
@@ -17,6 +23,8 @@ func _on_play_button_pressed():
 
 
 func _on_leaderboard_button_pressed():
+	$CreditsPanel.hide()
+	
 	var board_entry = preload("res://Scenes/Prototype/BoardEntry.tscn")
 	var scores: Dictionary = await SilentWolf.Scores.get_scores().sw_get_scores_complete
 	# LOAD SCORES
@@ -42,4 +50,20 @@ func _on_leaderboard_close_button_pressed():
 
 
 func _on_borus_interaction_button_down():
+	var sfx = borus_click_sfx.pick_random()
+	$AudioStreamPlayer.stream = sfx
 	$AudioStreamPlayer.play()
+	$BorusSprite.texture = borus_sweat_sprite
+
+
+func _on_borus_interaction_button_up():
+	$BorusSprite.texture = borus_normal_sprite
+
+
+func _on_credits_button_pressed():
+	$LeaderboardPanel.hide()
+	$CreditsPanel.show()
+
+
+func _on_credits_close_button_pressed():
+	$CreditsPanel.hide()
