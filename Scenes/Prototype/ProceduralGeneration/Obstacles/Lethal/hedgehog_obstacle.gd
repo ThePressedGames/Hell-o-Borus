c602bf391df2144bdd084958a156535ca9054eb4
Marco Dugatto <dughi.boogie@gmail.com>
2024-01-26 14:50:13 +0100

    Tweaked audio volumes for obstacles, player and music

--- Scenes/Prototype/ProceduralGeneration/Obstacles/Lethal/hedgehog_obstacle.gd
+++ Scenes/Prototype/ProceduralGeneration/Obstacles/Lethal/hedgehog_obstacle.gd
@@ -4,7 +4,7 @@ extends LethalObstacle
 @export var spawn_sounds:Array[AudioStreamMP3]
 
 
-func _ready():
+func _on_visible_on_screen_notifier_2d_screen_entered():
 	var spawn_sfx = spawn_sounds.pick_random()
 	$AudioStreamPlayer.stream = spawn_sfx
 	$AudioStreamPlayer.play()
