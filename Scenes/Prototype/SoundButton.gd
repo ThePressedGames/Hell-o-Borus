d6ff1b96c4320618d781787cd08746b2c7125e43
Marco Dugatto <dughi.boogie@gmail.com>
2024-01-26 19:14:51 +0100

    Fixed small bug on sound button

--- Scenes/Prototype/SoundButton.gd
+++ Scenes/Prototype/SoundButton.gd
@@ -5,11 +5,6 @@ var music_bus = AudioServer.get_bus_index("Master")
 func _on_pressed():
 	AudioServer.set_bus_mute(music_bus, not AudioServer.is_bus_mute(music_bus))
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	button_pressed = AudioServer.is_bus_mute(music_bus)
