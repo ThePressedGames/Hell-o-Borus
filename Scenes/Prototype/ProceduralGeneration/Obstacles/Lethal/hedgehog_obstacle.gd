1d2ae698e3ae3bbcd75a9257e61d48081dc92bd1
Marco Dugatto <dughi.boogie@gmail.com>
2024-01-26 11:50:48 +0100

    Added updated sfx for player and enemies

--- Scenes/Prototype/ProceduralGeneration/Obstacles/Lethal/hedgehog_obstacle.gd
+++ Scenes/Prototype/ProceduralGeneration/Obstacles/Lethal/hedgehog_obstacle.gd
@@ -1,5 +1,14 @@
 extends LethalObstacle
 
 
+@export var spawn_sounds:Array[AudioStreamMP3]
+
+
+func _ready():
+	var spawn_sfx = spawn_sounds.pick_random()
+	$AudioStreamPlayer.stream = spawn_sfx
+	$AudioStreamPlayer.play()
+
+
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
